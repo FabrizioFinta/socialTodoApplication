9253aaee922b0497ad298822a2074cfd4b7cb98c
Fabrizio <fabrizio.finta@gmail.com>
2018-01-25 15:05:45 +0100

    init(SocialTodoApp): project initialized, gitignore file added

---
+++ src/main/java/com/greenfoxacademy/socialdotoapp/SocialdotoappApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.socialdotoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialdotoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialdotoappApplication.class, args);
+	}
+}
